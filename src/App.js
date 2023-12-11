dacc03377ca3a382ba8c5b9d77fb69950deb5450
Makinde Oluwasegun <bryanconquer@gmail.com>
2023-12-11 18:50:26 +0100

    wallet-connect

--- src/App.js
+++ src/App.js
@@ -1,22 +1,68 @@
-import logo from './logo.svg';
+import {useEffect, useState} from "react"
 import './App.css';
 
+
 function App() {
+  const [walletaddress, setWalletAddress] = useState('');
+
+  useEffect (() => {
+    currentAcct();
+    accListener ();
+  })
+
+  const walletConnect = async () => {
+    if ( typeof window != "undefined" && typeof window.ethereum != "undefined") {
+      try {
+      const accounts = await window.ethereum.request({method: "eth_requestAccounts"})
+      setWalletAddress(accounts[0])
+      console.log(accounts[0])
+
+      }catch (e){
+        console.log(e)
+      }
+    }else{
+      console.log("Install metamask extention")
+    }
+  }
+  
+  
+  const currentAcct = async () => {
+    if ( typeof window != "undefined" && typeof window.ethereum != "undefined") {
+      try {
+      const accounts = await window.ethereum.request({method: "eth_accounts"})
+
+      if (accounts.length > 0) {
+        setWalletAddress(accounts[0])
+        console.log(accounts[0])
+      }else{
+        console.log("connect with connect button")
+      }
+
+      }catch (e){
+        console.log(e)
+      }
+    }else{
+      console.log("Install metamask extention")
+    }
+  }
+
+  const accListener = async () => {
+    if ( typeof window != "undefined" && typeof window.ethereum != "undefined") {
+      window.ethereum.on("accountsChanged", (accounts) => {
+        setWalletAddress(accounts[0])
+        console.log(accounts[0])
+      })
+    }else{
+      console.log("Install metamask extention")
+    }
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+       <h1>BriTech</h1>
+        <p>Connect Your Wallet</p>
+        <button onClick={walletConnect}>{walletaddress && walletaddress.length > 0 ? `connected ${walletaddress.substring(0,4)}....${walletaddress.substring(38) }` : "connect wallet"} </button>
       </header>
     </div>
   );
